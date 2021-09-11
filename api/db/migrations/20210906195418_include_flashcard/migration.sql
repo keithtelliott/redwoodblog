100265cbc85d47a6a66ab3d528fcb071b640b99e
Keith T Elliott <keith.t.elliott@gmail.com>
2021-09-10 22:23:00 -0500

    Remove prisma migrations folder, so the postgresql build will not fail.

--- api/db/migrations/20210906195418_include_flashcard/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateTable
-CREATE TABLE "Flashcard" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "front" TEXT NOT NULL,
-    "back" TEXT NOT NULL,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
